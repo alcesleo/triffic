d14eca0581a744072af00f8ae43a6aa2612bb639
alcesleo <lagginglion@gmail.com>
2013-12-18 17:19:25 +0100

    Showing Sweden to begin with

--- src/maps.js
+++ src/maps.js
@@ -3,9 +3,9 @@ var triffic = triffic || {};
 triffic.maps = {}
 
 triffic.maps.load = function () {
-    var myLatlng = new google.maps.LatLng(10, 10);
+    var myLatlng = new google.maps.LatLng(62, 15);
     var mapOptions = {
-        zoom: 4,
+        zoom: 6,
         center: myLatlng
     }
     triffic.maps.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
